09a17141559e74da6b6f1787e788fbf6adf880d9
Mauricio Coronel <cmauriciocoronel@gmail.com>
2021-07-16 21:53:43 -0300

    Setup

--- ipfs/update.sh
+++ ipfs/update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-docker exec efem-ipfs ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials '["true"]'
-docker exec efem-ipfs ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
-docker exec efem-ipfs ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
-docker exec efem-ipfs ipfs config --json Gateway.HTTPHeaders.Access-Control-Allow-Credentials '["true"]'
-docker exec efem-ipfs ipfs config --json Gateway.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
-docker exec efem-ipfs ipfs config --json Gateway.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST", "OPTIONS"]'
-docker exec efem-ipfs ipfs config --json Gateway.HTTPHeaders.Access-Control-Expose-Headers '["Ipfs-Hash"]'
-docker exec efem-ipfs ipfs config --json Gateway.Writable true
-docker restart efem-ipfs
-docker exec efem-ipfs ipfs config show
+docker exec avaldao-ipfs ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials '["true"]'
+docker exec avaldao-ipfs ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
+docker exec avaldao-ipfs ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST"]'
+docker exec avaldao-ipfs ipfs config --json Gateway.HTTPHeaders.Access-Control-Allow-Credentials '["true"]'
+docker exec avaldao-ipfs ipfs config --json Gateway.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
+docker exec avaldao-ipfs ipfs config --json Gateway.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "GET", "POST", "OPTIONS"]'
+docker exec avaldao-ipfs ipfs config --json Gateway.HTTPHeaders.Access-Control-Expose-Headers '["Ipfs-Hash"]'
+docker exec avaldao-ipfs ipfs config --json Gateway.Writable true
+docker restart avaldao-ipfs
+docker exec avaldao-ipfs ipfs config show
